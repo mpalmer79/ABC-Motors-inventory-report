633c5fe61222c9efae1b35045af262179b2d00b2
mpalmer79 <mpalm2025@gmail.com>
2026-02-03 11:34:27 -0500

    Create HeaderBar.tsx

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,51 @@
+// src/components/HeaderBar.tsx
+import React, { FC } from "react";
+
+interface Props {
+    searchTerm: string;
+    onSearchChange: (value: string) => void;
+}
+
+export const HeaderBar: FC<Props> = () => {
+    return (
+          <header className="relative overflow-hidden">
+            {/* Mobile/Tablet Image - shown below md breakpoint (768px) */}
+                <div
+                          className="md:hidden relative h-[180px] sm:h-[220px] bg-no-repeat"
+                          style={{
+                                      backgroundImage: "url('/chevy-showroom-mobile.png')",
+                                      backgroundPosition: "center top",
+                                      backgroundSize: "contain"
+                          }}
+                        >
+                  {/* Light overlay */}
+                        <div className="absolute inset-0 bg-white/5" />
+                  {/* Bottom fade gradient */}
+                        <div
+                                    className="absolute bottom-0 left-0 right-0 h-[60px] sm:h-[80px]"
+                                    style={{
+                                                  background: "linear-gradient(to bottom, transparent 0%, hsl(var(--background)) 100%)"
+                                    }}
+                                  />
+                </div>div>
+          
+            {/* Desktop Image - shown at md breakpoint and above (768px+) */}
+                <div
+                          className="hidden md:block relative h-[380px] bg-cover bg-top bg-no-repeat"
+                          style={{
+                                      backgroundImage: "url('/chevy-showroom.png')"
+                          }}
+                        >
+                  {/* Light overlay */}
+                        <div className="absolute inset-0 bg-white/5" />
+                  {/* Bottom fade gradient */}
+                        <div
+                                    className="absolute bottom-0 left-0 right-0 h-[150px]"
+                                    style={{
+                                                  background: "linear-gradient(to bottom, transparent 0%, hsl(var(--background)) 100%)"
+                                    }}
+                                  />
+                </div>div>
+          </header>header>
+        );
+};</header>
