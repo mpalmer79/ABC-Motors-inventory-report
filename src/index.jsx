59cc7248bc3345f56e703a5759568312f9069b91
mpalmer79 <mpalm2025@gmail.com>
2026-02-03 11:29:24 -0500

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+// src/index.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "./context/ThemeContext";
+import "./styles/theme.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ThemeProvider>
+              <App />
+        </ThemeProvider>ThemeProvider>
+    </React.StrictMode>React.StrictMode>
+  );</React.StrictMode>
