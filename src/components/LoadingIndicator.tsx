f51f0405fd06bbafaf5650e4c08f5362e2a35e66
mpalmer79 <mpalm2025@gmail.com>
2026-02-03 11:53:19 -0500

    Create LoadingIndicator.tsx

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,31 @@
+// src/components/LoadingIndicator.tsx
+import React, { FC } from "react";
+
+interface Props {
+    message?: string;
+    size?: "small" | "medium" | "large";
+}
+
+export const LoadingIndicator: FC<Props> = ({
+    message = "Loading...",
+    size = "medium",
+}) => {
+    const sizeMap = {
+          small: 24,
+          medium: 40,
+          large: 56,
+    };
+
+    return (
+          <div className="loading-container">
+                <div
+                          className="loading-spinner"
+                          style={{
+                                      width: sizeMap[size],
+                                      height: sizeMap[size],
+                          }}
+                        />
+            {message && <div className="loading-text">{message}</div>div>}
+          </div>div>
+        );
+};</div>
